6742b525d18fa7a37015b06e237c759fff6145fb
smartboy84 <www.hamdanmahmood12@gmail.com>
2025-04-21 12:11:25 +0800

    ting a ling

--- lib/sdcard/sdcard.cpp
+++ lib/sdcard/sdcard.cpp
@@ -9,17 +9,22 @@
 
 File logFile;
 
-bool createDir(fs::FS &fs, const char *path) {
-  if (fs.mkdir(path)) {
+bool createDir(fs::FS &fs, const char *path)
+{
+  if (fs.mkdir(path))
+  {
     ESP_LOGI(TAG, "Dir %s created", path);
     return true;
-  } else {
+  }
+  else
+  {
     ESP_LOGI(TAG, "mkdir failed");
     return false;
   }
 }
 
-bool dirExists(fs::FS &fs, const char *dirname) {
+bool dirExists(fs::FS &fs, const char *dirname)
+{
   File root = fs.open(dirname);
 
   if (!root)
@@ -30,7 +35,8 @@ bool dirExists(fs::FS &fs, const char *dirname) {
   return isDir;
 }
 
-bool writeFile(fs::FS &fs, const char *path, const char *message) {
+bool writeFile(fs::FS &fs, const char *path, const char *message)
+{
   File file = fs.open(path, FILE_WRITE);
   if (!file)
     return false;
@@ -41,27 +47,58 @@ bool writeFile(fs::FS &fs, const char *path, const char *message) {
     return false;
 }
 
-bool openLogFile(fs::FS &fs, const char *path) {
+bool openLogFile(fs::FS &fs, const char *path)
+{
   logFile = fs.open(path, FILE_APPEND);
-  if (!logFile) {
+  if (!logFile)
+  {
     ESP_LOGE(TAG, "Failed to open file for appending");
     return false;
   }
   return true;
 }
 
-bool appendToOpenFile(const char *message) {
-  if (!logFile) return false;
-  if (logFile.print(message)) return true;
+bool writeRawHeader()
+{
+  if (!logFile)
+    return false;
+
+  if (logFile.write((uint8_t *)&fileHeader, 1))
+    return true;
+
+  ESP_LOGE(TAG, "header failed");
+}
+
+bool appendRawToOpenFile(const EntryType dtype, const uint8_t *buf, const uint8_t n)
+{
+  if (!logFile)
+    return false;
+
+  if (logFile.write(static_cast<uint8_t>(dtype)) && logFile.write(buf, n))
+    return true;
+
+  ESP_LOGE(TAG, "Raw append failed");
+  return false;
+}
+
+bool appendToOpenFile(const char *message)
+{
+  if (!logFile)
+    return false;
+  if (logFile.print(message))
+    return true;
   ESP_LOGE(TAG, "Append failed");
   return false;
 }
 
-void flushLogFile() {
-  if (logFile) logFile.flush();
+void flushLogFile()
+{
+  if (logFile)
+    logFile.flush();
 }
 
-void incrementLogPath(char* logPath) {
+void incrementLogPath(char *logPath)
+{
   size_t baseDirNameLen = strlen(BASE_DIR_NAME);
   int number;
   sscanf(logPath + baseDirNameLen, "%4d", &number);
@@ -71,19 +108,23 @@ void incrementLogPath(char* logPath) {
 char logPath[sizeof(BASE_DIR_NAME) + 4] = BASE_DIR_NAME "0000";
 char filePath[2 * sizeof(logPath) + 3];
 
-SetupStatus setupSdCard(uint8_t cmd, uint8_t clk, uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3) {
-  if (!SD_MMC.setPins(clk, cmd, d0, d1, d2, d3)) {
+SetupStatus setupSdCard(uint8_t cmd, uint8_t clk, uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3)
+{
+  if (!SD_MMC.setPins(clk, cmd, d0, d1, d2, d3))
+  {
     ESP_LOGE(TAG, "Failed to change SD card pins");
     return SDCARD_ERROR;
   }
 
-  if (!SD_MMC.begin()) {
+  if (!SD_MMC.begin())
+  {
     ESP_LOGE(TAG, "Failed to mount SD card");
     return SDCARD_ERROR;
   }
 
   uint8_t cardType = SD_MMC.cardType();
-  if (cardType == CARD_NONE) {
+  if (cardType == CARD_NONE)
+  {
     ESP_LOGE(TAG, "No SD card inserted");
     return SDCARD_ERROR;
   }
@@ -92,9 +133,12 @@ SetupStatus setupSdCard(uint8_t cmd, uint8_t clk, uint8_t d0, uint8_t d1, uint8_
   printf("SD_MMC Card Size: %lluMB\n", cardSize);
 
   // Create base folder for this logging instance
-  for (uint32_t i = 0; i < 10000; i++) {
-    if (!dirExists(SD_MMC, logPath)) {
-      if (!createDir(SD_MMC, logPath)) {
+  for (uint32_t i = 0; i < 10000; i++)
+  {
+    if (!dirExists(SD_MMC, logPath))
+    {
+      if (!createDir(SD_MMC, logPath))
+      {
         ESP_LOGE(TAG, "Failed to create base folder");
         return SDCARD_ERROR;
       }
@@ -104,18 +148,20 @@ SetupStatus setupSdCard(uint8_t cmd, uint8_t clk, uint8_t d0, uint8_t d1, uint8_
 
     incrementLogPath(logPath);
 
-    if (i == 9999) {
+    if (i == 9999)
+    {
       ESP_LOGE(TAG, "Folder count limit reached");
       return SDCARD_ERROR;
     }
   }
 
-  snprintf(filePath, sizeof(filePath), "%s%s.csv", logPath, logPath);
-  const char* header = "timestamp,verticalVelocity,accelX,accelY,accelZ,rawAccelX,rawAccelY,rawAccelZ,gyroX,gyroY,gyroZ,rawGyroX,rawGyroY,rawGyroZ,quatnW,quatnX,quatnY,quatnZ\n";
-  if (!writeFile(SD_MMC, filePath, header)) {
-    ESP_LOGE(TAG, "Could not create log file");
-    return SDCARD_ERROR;
-  }
+  snprintf(filePath, sizeof(filePath), "%s%s.bin", logPath, logPath);
+  // const char *header = "timestamp,verticalVelocity,accelX,accelY,accelZ,rawAccelX,rawAccelY,rawAccelZ,gyroX,gyroY,gyroZ,rawGyroX,rawGyroY,rawGyroZ,quatnW,quatnX,quatnY,quatnZ\n";
+  // if (!writeFile(SD_MMC, filePath, header))
+  // {
+  //   ESP_LOGE(TAG, "Could not create log file");
+  //   return SDCARD_ERROR;
+  // }
 
   ESP_LOGI(TAG, "SD card setup successful");
   return SETUP_OK;
@@ -126,62 +172,87 @@ int strPosn = 0;
 const uint8_t base = 10;
 const char cs = ',';
 
-void writeIntData(int dataValue) {
+void writeIntData(int dataValue)
+{
   itoa(dataValue, dataString + strPosn, base);
-  while(dataString[strPosn]!= '\0'){strPosn++;}
+  while (dataString[strPosn] != '\0')
+  {
+    strPosn++;
+  }
   dataString[strPosn] = cs;
   strPosn++;
 }
 
-void writeULongData(unsigned long dataValue){
+void writeULongData(unsigned long dataValue)
+{
   ultoa(dataValue, dataString + strPosn, base);
-  while(dataString[strPosn]!= '\0'){strPosn++;}
+  while (dataString[strPosn] != '\0')
+  {
+    strPosn++;
+  }
   dataString[strPosn] = cs;
   strPosn++;
 }
 
-void writeLongData(long dataValue){
+void writeLongData(long dataValue)
+{
   ltoa(dataValue, dataString + strPosn, base);
-  while(dataString[strPosn]!= '\0'){strPosn++;}
+  while (dataString[strPosn] != '\0')
+  {
+    strPosn++;
+  }
   dataString[strPosn] = cs;
   strPosn++;
 }
 
-void writeFloatData(float dataValue, byte decimals){
+void writeFloatData(float dataValue, byte decimals)
+{
   long fracInt;
   float partial;
 
-  //sign portion
-  if (dataValue < 0) {
-    dataString[strPosn] = '-'; 
+  // sign portion
+  if (dataValue < 0)
+  {
+    dataString[strPosn] = '-';
     strPosn++;
     dataValue *= -1;
   }
-  
-  //integer portion
+
+  // integer portion
   itoa((int)dataValue, dataString + strPosn, base);
-  while(dataString[strPosn]!= '\0'){ strPosn++; }
-  dataString[strPosn]='.'; strPosn++;
-  
-  //fractional portion
-  partial = dataValue - (int)(dataValue);
-  fracInt = (long)(partial*powf(10,decimals));
-  if (fracInt == 0) {
-    dataString[strPosn] = '0'; 
-    strPosn++; 
-    dataString[strPosn] = cs; 
+  while (dataString[strPosn] != '\0')
+  {
     strPosn++;
   }
-  else {
+  dataString[strPosn] = '.';
+  strPosn++;
+
+  // fractional portion
+  partial = dataValue - (int)(dataValue);
+  fracInt = (long)(partial * powf(10, decimals));
+  if (fracInt == 0)
+  {
+    dataString[strPosn] = '0';
+    strPosn++;
+    dataString[strPosn] = cs;
+    strPosn++;
+  }
+  else
+  {
     decimals--;
-    while(fracInt < powf(10, decimals)){ 
-      dataString[strPosn]='0';
+    while (fracInt < powf(10, decimals))
+    {
+      dataString[strPosn] = '0';
       strPosn++;
-      decimals--; 
+      decimals--;
     }
     ltoa(fracInt, dataString + strPosn, base);
-    while(dataString[strPosn]!= '\0'){ strPosn++; }
-    dataString[strPosn]=','; strPosn++;
+    while (dataString[strPosn] != '\0')
+    {
+      strPosn++;
+    }
+    dataString[strPosn] = ',';
+    strPosn++;
   }
 }
 
@@ -189,56 +260,59 @@ float logTime = 0;
 uint64_t lastLogTime = 0;
 uint64_t numLogs = 0;
 
-void LoggingTask(void* pvParameters) {
+float buf[32];
+
+void LoggingTask(void *pvParameters)
+{
   openLogFile(SD_MMC, filePath);
-  while (1) {
+  writeRawHeader();
+
+  while (1)
+  {
     EventBits_t bits = xEventGroupWaitBits(
-      sensorEventGroup,
-      IMU_SENSOR_EVENT | BARO_SENSOR_EVENT | GNSS_SENSOR_EVENT,
-      pdTRUE,
-      pdFALSE,
-      portMAX_DELAY
-    );
+        sensorEventGroup,
+        IMU_SENSOR_EVENT | BARO_SENSOR_EVENT | GNSS_SENSOR_EVENT,
+        pdTRUE,
+        pdFALSE,
+        portMAX_DELAY);
 
     uint64_t now = micros();
     float dt = (now - lastLogTime) * 1e-3;
     lastLogTime = now;
 
-    if (dt < 1000) logTime += dt;
+    if (dt < 1000)
+      logTime += dt;
 
-    writeFloatData(logTime, 2);
+    buf[0] = logTime;
 
-    writeFloatData(accelVertVel, 2);
-    writeFloatData(accelCorrected.x, 2);
-    writeFloatData(accelCorrected.y, 2);
-    writeFloatData(accelCorrected.z, 2);
-    writeFloatData(accelMs.x, 2);
-    writeFloatData(accelMs.y, 2);
-    writeFloatData(accelMs.z, 2);
+    buf[1] = accelVertVel;
+    buf[2] = accelCorrected.x;
+    buf[3] = accelCorrected.y;
+    buf[4] = accelCorrected.z;
+    buf[5] = accelMs.x;
+    buf[6] = accelMs.y;
+    buf[7] = accelMs.z;
 
-    writeFloatData(gyroCorrected.x, 2);
-    writeFloatData(gyroCorrected.y, 2);
-    writeFloatData(gyroCorrected.z, 2);
-    writeFloatData(gyroDps.x, 2);
-    writeFloatData(gyroDps.y, 2);
-    writeFloatData(gyroDps.z, 2);
-    writeFloatData(attitudeQuatn.w, 5);
-    writeFloatData(attitudeQuatn.v.x, 5);
-    writeFloatData(attitudeQuatn.v.y, 5);
-    writeFloatData(attitudeQuatn.v.z, 5);
+    buf[8] = gyroCorrected.x;
+    buf[9] = gyroCorrected.y;
+    buf[10] = gyroCorrected.z;
+    buf[11] = gyroDps.x;
+    buf[12] = gyroDps.y;
+    buf[13] = gyroDps.z;
 
-    strPosn--;
-    dataString[strPosn] = '\n'; strPosn++;
-    dataString[strPosn] = '\0';
+    buf[14] = attitudeQuatn.w;
+    buf[15] = attitudeQuatn.v.x;
+    buf[16] = attitudeQuatn.v.y;
+    buf[17] = attitudeQuatn.v.z;
 
-    appendToOpenFile(dataString);
-    strPosn = 0;
+    appendRawToOpenFile(EntryType::IMU, (uint8_t *)&buf, sizeof(float) * 17);
 
-    if (numLogs > 4000) { // Save to file every ~10 seconds
+    if (numLogs > 4000)
+    { // Save to file every ~10 seconds
       flushLogFile();
       numLogs = 0;
     }
-    
+
     numLogs++;
   }
 }
--- lib/sdcard/sdcard.h
+++ lib/sdcard/sdcard.h
@@ -8,6 +8,74 @@
 bool writeFile(fs::FS &fs, const char *path, const char *message);
 bool appendFile(fs::FS &fs, const char *path, const char *message);
 SetupStatus setupSdCard(uint8_t cmd, uint8_t clk, uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3);
-void LoggingTask(void* pvParameters);
+void LoggingTask(void *pvParameters);
+
+// this way we won't need to touch the 'extractor' program *at all to add support for more data - did I miss any?
+enum class DataType : uint8_t
+{
+    INT = 0,
+    FLOAT = 1,
+    BOOL = 2
+};
+
+enum class EntryType : uint8_t
+{
+    IMU = 0,
+    BARO = 1,
+    SENSOR = 2
+};
+
+typedef struct __attribute__((packed))
+{
+    char name[32]; // data type string identifier (for csv output)
+    DataType type;
+} DataEntry;
+
+// using struct in case I want to add more settings
+typedef struct __attribute__((packed))
+{
+    char name[32]; // this name is used in extractor program to extract data (--name "imu")
+    DataEntry dataOrder[24];
+    uint8_t dataOrderSize; // maybe a 'DataType::END' instead?
+    EntryType id;          // to link entries to this
+} sensor_output_header;
+
+// IMU sensor data
+sensor_output_header fileHeader[] = {
+    {.name = "imu", .dataOrder = {
+                        {"timestamp", DataType::FLOAT}, // timestamp
+
+                        // verticalVelocity
+                        {"verticalVelocity", DataType::FLOAT},
+
+                        // accelX, accelY, accelZ
+                        {"accelX", DataType::FLOAT},
+                        {"accelY", DataType::FLOAT},
+                        {"accelZ", DataType::FLOAT},
+
+                        // rawAccelX, rawAccelY, rawAccelZ
+                        {"rawAccelX", DataType::FLOAT},
+                        {"rawAccelY", DataType::FLOAT},
+                        {"rawAccelZ", DataType::FLOAT},
+
+                        // gyroX, gyroY, gyroZ
+                        {"gyroX", DataType::FLOAT},
+                        {"gyroY", DataType::FLOAT},
+                        {"gyroZ", DataType::FLOAT},
+
+                        // rawGyroX, rawGyroY, rawGyroZ
+                        {"rawGyroX", DataType::FLOAT},
+                        {"rawGyroY", DataType::FLOAT},
+                        {"rawGyroZ", DataType::FLOAT},
+
+                        // quatnW, quatnX, quatnY, quatnZ
+                        {"quatnW", DataType::FLOAT},
+                        {"quatnX", DataType::FLOAT},
+                        {"quatnY", DataType::FLOAT},
+                        {"quatnZ", DataType::FLOAT},
+                    },
+     .dataOrderSize = 18,
+     .id = EntryType::IMU},
+};
 
 #endif
